daae9d903cb9329018efae59d3ff5ab23a95b098
s368 <s368s368s@gmail.com>
2018-03-03 14:31:55 +0100

    TreeNode implementation.

---
+++ src/treeNode/SampleData.groovy
@@ -0,0 +1,45 @@
+package treeNode;
+
+class SampleData {
+	
+		public static TreeNode<String> getSet1() {
+			TreeNode<String> root = new TreeNode<String>("root");
+one:			{
+				TreeNode<String> node0 = root.addChild("node0");
+				TreeNode<String> node1 = root.addChild("node1");
+				TreeNode<String> node2 = root.addChild("node2");
+two:				{
+					TreeNode<String> node20 = node2.addChild(null);
+					TreeNode<String> node21 = node2.addChild("node21");
+three:					{
+						TreeNode<String> node210 = node21.addChild("node210");
+						TreeNode<String> node211 = node21.addChild("node211");
+					}
+				}
+				TreeNode<String> node3 = root.addChild("node3");
+four:				{
+					TreeNode<String> node30 = node3.addChild("node30");
+				}
+			}
+	
+			return root;
+		}
+	
+		public static TreeNode<String> getSetSOF() {
+			TreeNode<String> root = new TreeNode<String>("root");
+one:			{
+				TreeNode<String> node0 = root.addChild("node0");
+				TreeNode<String> node1 = root.addChild("node1");
+				TreeNode<String> node2 = root.addChild("node2");
+two:				{
+					TreeNode<String> node20 = node2.addChild(null);
+					TreeNode<String> node21 = node2.addChild("node21");
+three:					{
+						TreeNode<String> node210 = node20.addChild("node210");
+					}
+				}
+			}
+	
+			return root;
+		}
+	}
---
+++ src/treeNode/TreeNodeIter.groovy
@@ -0,0 +1,5 @@
+package treeNode
+
+class TreeNodeIter {
+
+}
