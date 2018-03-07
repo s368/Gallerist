79d64896139c9802e0735f86bb67864aa1d1dc6f
s368 <s368s368s@gmail.com>
2018-03-07 23:13:26 +0100

    Template classes.

--- src/main/templateClassJava.java
+++ src/main/templateClassJava.java
@@ -4,9 +4,10 @@ public class templateClassJava {
 	public static void main(String[] args) 
 	{
 		// Creating a generic List collection
-		ListTypeJava<String> lststr = new ListTypeJava<>();
+		ListType<String> lststr = new ListType<>();
 		lststr.set("First String");
 		System.out.println(lststr.get());
+		System.out.println(lststr.localt);
 		  
 		ListTypeJava<Integer> lstint = new ListTypeJava<>();
 		lstint.set(1);
