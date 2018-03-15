2599099245305fef6f186ac8776a5f5c7cf72b3a
s368 <s368s368s@gmail.com>
2018-03-15 16:52:18 +0100

    Artist.

--- src/gameResources/Artist/ArtistCard.groovy
+++ src/gameResources/Artist/ArtistCard.groovy
@@ -4,7 +4,7 @@ import gameResources.ColorType
 import gameResources.FameStars
 import gameResources.workOfArt.ArtType
 
-class Artist {
+class ArtistCard {
 	
 	//closed side
 	public int defaultFameIndex;
@@ -17,4 +17,19 @@ class Artist {
 	//opened side
 	public int currentFameIndex;
 	public FameRoad fameRoad;
+	
+	public ArtistCard(ArtistCard v)
+	{
+		if(v != null)
+		{
+			this.defaultFameIndex = v.defaultFameIndex;
+			this.defaultFameStars = v.defaultFameStars;
+			this.promo = v.promo;
+			
+			this.art = v.art;
+			this.color = v.color;
+
+			this.fameRoad = v.fameRoad;
+		}
+	}
 }
